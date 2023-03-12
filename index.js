21381dad3790ade7f2ae6d4e216fcf0df0de8867
Rockranx <sughtera@gmail.com>
2023-03-12 15:13:52 +0100

    d

--- index.js
+++ index.js
@@ -1,67 +1,66 @@
-// require("dotenv").config();
+require("dotenv").config();
 
-// // Require the framework and instantiate it
-// const fastify = require("fastify")({ logger: true });
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+// Require the framework and instantiate it
+const fastify = require("fastify")({ logger: true });
+const stripe = require("stripe")(`${process.env.STRIPE_SECRET_KEY}`);
 
-// const PORT = process.env.PORT || 5252;
-// // Fetch the publishable key to initialize Stripe.js
-// fastify.get("/publishable-key", () => {
-//   return { publishable_key: process.env.STRIPE_PUBLISHABLE_KEY };
-// });
+const PORT = process.env.PORT || 5252;
+// Fetch the publishable key to initialize Stripe.js
+fastify.get("/publishable-key", () => {
+  return { publishable_key: `${process.env.STRIPE_PUBLISHABLE_KEY}` };
+});
+fastify.post("/", (res, req) => {
+    res.send("this is the server")
+})
 
-// // Create a payment intent and return its client secret
-// fastify.post("/create-payment-intent", async (req, res) => {
-//   let { amount } = req.body;
-//   try{
-//   const paymentIntent = await stripe.paymentIntents.create({
-//     amount,
-//     currency: "eur",
-//     automatic_payment_methods: {
-//       enabled: true,
-//     },
-//   });
-//   res.send({client_secret: paymentIntent.client_secret})
-//   } catch(error){
-//     res.status(500).send({ error: error.message });
-//   }
-// });
+// Create a payment intent and return its client secret
+fastify.post("/create-payment-intent", async (req, res) => {
+  let { amount } = req.body;
+  try{
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    currency: "eur",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+  res.send({client_secret: paymentIntent.client_secret})
+  } catch(error){
+    res.status(500).send({ error: error.message });
+  }
+});
 
-// // Run the server
-// const start = async () => {
-//   try {
-//     await fastify.listen({ port: PORT }, (err, address) => {
-//       if (err) throw err
-//       // Server is now listening on ${address}
-//     })
-//   } catch (err) {
-//     fastify.log.error(err);
-//     process.exit(1); 
-//   }
-// };
+// Run the server
+const start = async () => {
+  try {
+    await fastify.listen({ port: PORT }, (err, address) => {
+      if (err) throw err
+      // Server is now listening on ${address}
+    })
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1); 
+  }
+};
 
-// start();
+start();
 
 
 // const express = require("express")
 // const app =express()
 // require("dotenv").config();
-// const stripe = require("stripe")("sk_test_51Mcxe6DebByDEiAZ6ERRjfSb39RZaEPggp1nwSQTce4PP1Ij46BgqbNkJ1gOan1BdqJSevjRMEdkbLan9RWHS2gE006f8IDTby")
-// const bodyParser = require("body-parser")
-// const cors = require("cors")
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json())
-// app.use(cors())
-// app.use(express.static("dist"));
-// app.use(express.json());
 
-// app.post("/cart", async (req, res) => {
-//     console.log("started cart")
-//   const { items } = req.body;
+//  app.get("/publishable-key", () => {
+//    return { publishable_key: `${process.env.STRIPE_PUBLISHABLE_KEY}` };
+//  });
+
+// app.post("/create-payment-intent", async (req, res) => {
+//   const { amount } = req.body;
 //   try {
 //     const paymentIntent = await stripe.paymentIntents.create({
-//             amount: 30000,
+//             amount,
 //             currency: "usd",
 //             description: "mre",
 //             automatic_payment_methods: {
@@ -69,7 +68,7 @@
 //             },
 //           });
 //           res.send({
-//             clientSecret: paymentIntent.client_secret,
+//             client_secret: paymentIntent.client_secret
 //           });
 //   } catch (error) {
 //     console.log("Error", error);
@@ -86,28 +85,28 @@
 //   }
 // })
 
-// app.listen(process.env.PORT || 5173, () => {
-//   console.log("Sever is listening on port 5173");
+// app.listen(process.env.PORT || 5252, () => {
+//   console.log("Sever is listening on port 5252");
 // });
 
 
 
 
-// Importing express module
-const express = require("express")
-const app = express()
+// // Importing express module
+// const express = require("express")
+// const app = express()
   
-// Handling GET / request
-// app.use("/", (req, res, next) => {
-//     res.send("This is the express server")
+// // Handling GET / request
+// // app.use("/", (req, res, next) => {
+// //     res.send("This is the express server")
+// // })
+  
+// // Handling GET /hello request
+// app.get("/hello", (req, res, next) => {
+//     res.send("This is the hello response");
 // })
   
-// Handling GET /hello request
-app.get("/hello", (req, res, next) => {
-    res.send("This is the hello response");
-})
-  
-// Server setup
-app.listen(3000, () => {
-    console.log("Server is Running")
-})
+// // Server setup
+// app.listen(3000, () => {
+//     console.log("Server is Running")
+// })
